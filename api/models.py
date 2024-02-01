1380a4edb9f3c97e3f8ca43dbfb034140cb30bba
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-02-01 20:27:07 -0300

    chore: add model for temperature forecast

--- api/models.py
+++ api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class TemperatureForecast(models.Model):
+    temperature = models.FloatField()
+    atmospheric_pressure = models.FloatField()
+    humidity = models.FloatField()
+    precipitation_percentage = models.FloatField()
+    weather_conditions = models.CharField(max_length=150)
