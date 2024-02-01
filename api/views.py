cd82a78ad65d774d886208bbf25c8bccabce42af
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-02-01 20:59:10 -0300

    chore: add view for temperature forecast

--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import TemperatureForecast
+from .serializer import TemperatureForecast
 
 # Create your views here.
+class TemperatureForecastViewset(viewsets.ModelViewSet):
+    queryset = TemperatureForecast.objects.all()
+    serializer_class = TemperatureForecast
