e04cce453f03d0a47da9470feb1f299770cbbde5
Ana Julia M Martins <ana_juh17@hotmail.com>
2024-02-05 19:30:47 -0300

    fix: fix to right import view class

--- api/views.py
+++ api/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import TemperatureForecast
-from .serializer import TemperatureForecast
+from .serializer import TemperatureForecastSerializer
 
 # Create your views here.
 class TemperatureForecastViewset(viewsets.ModelViewSet):
     queryset = TemperatureForecast.objects.all()
-    serializer_class = TemperatureForecast
+    serializer_class = TemperatureForecastSerializer
