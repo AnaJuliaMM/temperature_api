ddc470a5e5ce1a579d5161e5d226b964f9511eaa
Ana Julia M Martins <ana_juh17@hotmail.com>
2024-02-06 16:12:29 -0300

    chore: remove code related to modelviewset view

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from api.views import TemperatureForecastViewset
+from . import views
 
-router = DefaultRouter()
-router.register('', TemperatureForecastViewset)
+# CBV - ModelViewSet
+# from rest_framework.routers import DefaultRouter
+# from api.views import TemperatureForecastViewset
+
+# router = DefaultRouter()
+# router.register('', TemperatureForecastViewset)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', views.index, name="login")
 ]
