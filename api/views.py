349d6a687c67345481fdf82edf3510b6c2054787
Ana Julia M Martins <ana_juh17@hotmail.com>
2024-02-06 16:13:40 -0300

    chore: implemente fbv to handle index request

--- api/views.py
+++ api/views.py
@@ -3,7 +3,12 @@ from rest_framework import viewsets
 from .models import TemperatureForecast
 from .serializer import TemperatureForecastSerializer
 
-# Create your views here.
-class TemperatureForecastViewset(viewsets.ModelViewSet):
-    queryset = TemperatureForecast.objects.all()
-    serializer_class = TemperatureForecastSerializer
+# CBV - ModelViewSet
+# class TemperatureForecastViewset(viewsets.ModelViewSet):
+#     queryset = TemperatureForecast.objects.all()
+#     serializer_class = TemperatureForecastSerializer
+
+def index(request):
+    latest_question_list = TemperatureForecast.objects.all()
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "api/index.html", context)
