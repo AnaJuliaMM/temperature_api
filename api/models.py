1194378a002400538fdf5791a6c4b483f2ef44ae
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-03-01 21:09:36 -0300

    chore: turn model into a generic entity

--- api/models.py
+++ api/models.py
@@ -1,10 +1,21 @@
-from django.db import models
 
-# Create your models here.
-class TemperatureForecast(models.Model):
-    temperature = models.FloatField()
-    atmospheric_pressure = models.FloatField()
-    humidity = models.FloatField()
-    precipitation_percentage = models.FloatField()
-    weather_conditions = models.CharField(max_length=150)
-    date_tiime = models.DateTimeField()
+# Create a generic model
+class WeatherEntity:
+
+    def __init__(self, temperature, date_time, atmospheric_pressure='', humidity='', precipitation_percentage='', weather_conditions='',city_name="Sorocaba" ) -> None:
+        self.city_name = city_name
+        self.temperature = temperature
+        self.atmospheric_pressure = atmospheric_pressure
+        self.humidity = humidity
+        self.precipitation_percentage = precipitation_percentage
+        self.weather_conditions = weather_conditions
+        self.date_time = date_time
+
+    def __str__(self) -> str:
+        return f"Weather Report for {self.city_name}:\n" \
+            f"Temperature: {self.temperature} °C\n" \
+            f"Atmospheric Pressure: {self.atmospheric_pressure} hPa\n" \
+            f"Humidity: {self.humidity}%\n" \
+            f"Precipitation Percentage: {self.precipitation_percentage}%\n" \
+            f"Weather Conditions: {', '.join(self.weather_conditions)}\n" \
+            f"Date and Time: {self.date_time}"
