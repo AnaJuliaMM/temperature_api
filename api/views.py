2127a32730d8bd546c4fccb228a1b16b3c6cfb1f
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-03-01 21:10:49 -0300

    chore: add model generater

--- api/views.py
+++ api/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
-from rest_framework import viewsets
-from .models import TemperatureForecast
+from rest_framework import views
+from .models import WeatherEntity
 from .serializer import TemperatureForecastSerializer
+from  django.http import HttpResponse
+from random import randrange
+from datetime import datetime
 
-# CBV - ModelViewSet
-# class TemperatureForecastViewset(viewsets.ModelViewSet):
-#     queryset = TemperatureForecast.objects.all()
-#     serializer_class = TemperatureForecastSerializer
 
-# View to render forecast list page
-def index(request):
-    return render(request, "api/forecasts.html")
+
+class WeatherView(views.View):        
+    def get(request):
+        weathers = []
+        for i in range(10):
+            weathers.append(
+                WeatherEntity(
+                    temperature=randrange(start=17, stop=40),
+                    date_time=datetime.now()
+                ))
+        return render(request, "api/forecasts.html")
+
