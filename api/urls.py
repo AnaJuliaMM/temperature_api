f964abfe652552a95237451b905fb3a6f18c3b02
AnaJuliaMM <ana.martins_2004@outlook.com>
2024-03-01 21:10:29 -0300

    fix: chane import in view

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from . import views
+from .views import WeatherView
 
-# CBV - ModelViewSet
-# from rest_framework.routers import DefaultRouter
-# from api.views import TemperatureForecastViewset
-
-# router = DefaultRouter()
-# router.register('', TemperatureForecastViewset)
 
 urlpatterns = [
-    path('', views.index, name="login")
+    path('', WeatherView.get, name="login")
 ]
